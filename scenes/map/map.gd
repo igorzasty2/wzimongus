7ed83232eff4fce2fc0d3db3c570de6f06b9c2f7
iljuhenson <ilyapavluk24@gmail.com>
2023-11-10 23:42:11 +0100

    Add player spawner on map for up to 3 players at the same time

---
+++ scenes/map/map.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+@export var PlayerScene : PackedScene
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var spawnPoints = $SpawnPoints.get_children()
+	var index = 0
+	for i in GameManager.Players:
+		var currentPlayer = PlayerScene.instantiate()
+		currentPlayer.name = str(GameManager.Players[i].id)
+		add_child(currentPlayer)
+		print(spawnPoints[index])
+		currentPlayer.global_position = spawnPoints[index].global_position
+		
+		index += 1
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -6,18 +6,22 @@ const SPEED = 300.0
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+func _ready():
+	$MultiplayerSynchronizer.set_multiplayer_authority(str(name).to_int())
 
 func _physics_process(delta):
-	var direction_y = Input.get_axis("ui_up", "ui_down")
-	if direction_y:
-		velocity.y = direction_y * SPEED
-	else:
-		velocity.y = move_toward(velocity.y, 0, SPEED)
-	
-	var direction_x = Input.get_axis("ui_left", "ui_right")
-	if direction_x:
-		velocity.x = direction_x * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+	if $MultiplayerSynchronizer.get_multiplayer_authority() == multiplayer.get_unique_id():
+		
+		var direction_y = Input.get_axis("ui_up", "ui_down")
+		if direction_y:
+			velocity.y = direction_y * SPEED
+		else:
+			velocity.y = move_toward(velocity.y, 0, SPEED)
+		
+		var direction_x = Input.get_axis("ui_left", "ui_right")
+		if direction_x:
+			velocity.x = direction_x * SPEED
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
 
-	move_and_slide()
+		move_and_slide()
