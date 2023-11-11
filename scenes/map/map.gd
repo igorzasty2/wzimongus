3070f7aa855bcdb20a0e3d52e8a3b6dc08e3f883
iljuhenson <ilyapavluk24@gmail.com>
2023-11-11 13:02:21 +0100

    Add documentation to map.gd script

--- scenes/map/map.gd
+++ scenes/map/map.gd
@@ -2,7 +2,13 @@ extends Node2D
 
 @export var PlayerScene : PackedScene
 
-# Called when the node enters the scene tree for the first time.
+## Called when the node enters the scene tree for the first time.
+##
+## On scene load looks up GameManager players dictionary then spawns them 
+## on every available spawn point.
+##
+## IMPORTANT: _ready() will throw an error if the amount of players is
+## greater than amount of spawn points.
 func _ready():
 	var spawnPoints = $SpawnPoints.get_children()
 	var index = 0
