95e11479f8ab3e19488162d526ab0dd84617ae41
iljuhenson <ilyapavluk24@gmail.com>
2023-11-11 13:40:25 +0100

    Add documentation to game_manager.gd script

--- globals/game_manager/game_manager.gd
+++ globals/game_manager/game_manager.gd
@@ -1,6 +1,13 @@
 extends Node
+## GameManager is more of a local database which stores
+## information about current server and its peers. GameManager
+## can be extended further to store the amount of tasks every player
+## has done, chat information can be stored here too in further development
+## of the game. 
 
+## Stores all players info who are connected to the server at the moment. 
 var Players = {}
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
