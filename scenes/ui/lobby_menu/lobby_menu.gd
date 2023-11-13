95e2112b556e3197bd8780d3e18dcabf5efbd25a
Dawid Maliszewski <dawid@thedavesky.com>
2023-11-13 16:45:24 +0100

    Improve lobby with server-side map spawner

--- scenes/ui/lobby_menu/lobby_menu.gd
+++ scenes/ui/lobby_menu/lobby_menu.gd
@@ -2,4 +2,31 @@ extends Control
 
 
 func _on_start_game_button_button_down():
-	MultiplayerManager.load_game.rpc("res://scenes/map/map.tscn")
+	# Tylko host jest w stanie rozpocząć grę
+	if multiplayer.is_server():
+		# Wysyłamy do klientów informację o rozpoczęciu gry
+		start_game.rpc()
+
+
+# Funkcja odpowiadająca za rozpoczęcie gry
+@rpc("call_local", "reliable")
+func start_game():
+	# Chowamy lobby
+	$LobbyUI.hide()
+
+	# Ładujemy mapę na serwerze, zostanie ona zsynchronizowana z klientami przez MapSpawner
+	if multiplayer.is_server():
+		change_map.call_deferred(load("res://scenes/map/map.tscn"))
+
+
+# Funkcja dla serwera odpowiadająca za zmianę mapy
+func change_map(scene: PackedScene):
+	var map = $Map
+
+	# Usuwamy obecną mapę
+	for i in map.get_children():
+		map.remove_child(i)
+		i.queue_free()
+	
+	# Wyświetlamy nową mapę
+	map.add_child(scene.instantiate())
