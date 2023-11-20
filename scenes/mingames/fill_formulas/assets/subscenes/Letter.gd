1c8eacdacdc6db59de54451744f064bb4cd0a657
Sebastian Matejak <sebastian.mat.2201@gmail.com>
2023-11-20 13:38:19 +0100

    Fixing bugs connected to moving letter blocks
    -Changed if statement that allows to move blocks to not allow summoning them after they returned to original position
    -Removed redundant variable
    -Made currentyly dragged block to always draw on top of other blocks

--- scenes/mingames/fill_formulas/assets/subscenes/Letter.gd
+++ scenes/mingames/fill_formulas/assets/subscenes/Letter.gd
@@ -3,9 +3,6 @@ extends StaticBody2D
 # Klasa funkcjonuje jako przesuwalne myszką pole z literą które umieścić należy
 # w odpowiednim polu
 
-# Zmienna przechowujące informacje o tym, czy gracz przytrzymuje myszę nad polem
-# z literą, konieczne do przmieszczania pola
-var mouse_on_object = false
 # original_position przetrzymuje informacje o początkowym położeniu pola, daje
 # możliwość przywrócenia tej pozycji po nieprawidłowym przesunięciu pola
 var original_position
@@ -15,6 +12,17 @@ var id = ""
 # placed informuje o tym, czy pole zostało już "położone" na odpowiednim miejscu
 # wzoru, służy do wyłączenia możliwości poruszania polem
 var placed = false
+# zmienna przetrzymująca oryginalną warstwę rysowania pola
+var orig_z_index = z_index
+# zmienna przechowująca wektor rozmiaru pola
+var size
+# zmienna przechowująca dokładne położenie pola (obszar przez niego zajmowany)
+var rect
+
+
+func _ready():
+	size = $Sprite2D.get_rect().size
+	rect = Rect2(position - size/2, size)
 
 
 func _process(delta):
@@ -23,10 +31,18 @@ func _process(delta):
 	# nie zostało ustawione na odpowiednim miejscu
 	if(
 		Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT) 
-		&& mouse_on_object 
+		&& get_parent().moving == self
 		&& placed == false
+		&& (position != original_position || get_parent().is_moving)
 	):
 		position = get_viewport().get_mouse_position()
+	# Służy do poprawnej zmiany wybranego pola z literą
+	if(
+		rect.has_point(get_viewport().get_mouse_position()) 
+		&& get_parent().is_moving == false
+		):
+		_on_mouse_entered()
+		
 
 # funkcja przywraca pole do pozycji oryginalnej
 func return_to_orig_pos():
@@ -37,13 +53,13 @@ func _on_mouse_entered():
 	# instrukcja warunkowa konieczna aby niemożliwe było podniesienie 
 	# jednocześnie więcej niż jednego pola
 	if (!get_parent().is_moving && placed == false):
-		mouse_on_object = true
 		get_parent().is_moving = true
 		get_parent().moving = self
+		z_index = 20
 
 
 func _on_mouse_exited():
 	# Przywraca możliwość podnoszenia innych pól z literami
 	if (get_parent().is_moving && get_parent().moving == self):
-		mouse_on_object = false
 		get_parent().is_moving = false
+		z_index = orig_z_index
