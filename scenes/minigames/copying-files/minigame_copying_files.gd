d9bfaf544325431a11a70902897f66e216086b5f
Patryk Matuszewski <patrykmatuszewski02@icloud.com>
2023-11-21 12:59:03 +0100

    added a copying files minigame
    logic and pre-design for a copying files minigame

---
+++ scenes/minigames/copying-files/minigame_copying_files.gd
@@ -0,0 +1,66 @@
+extends Button
+
+
+func _on_confirm_button_down():
+	# Załaduj grafikę
+	var texture = preload("res://assets/kopiowanie1.png")
+
+	# Ustaw grafikę w węźle tekstury
+	var texture_rect = TextureRect.new()
+	texture_rect.texture = texture
+
+	# Dodaj węzeł do sceny
+	add_child(texture_rect)
+
+	# Ustaw globalną pozycję tekstury
+	texture_rect.global_position = Vector2(222, 55)  # Dostosuj te współrzędne do pożądanej pozycji
+
+	# Oczekaj przez 0.5 sekundy
+	await get_tree().create_timer(0.6).timeout
+	# Załaduj drugą grafikę
+	var texture2 = preload("res://assets/kopiowanie2.png")
+	# Zmień teksturę na drugą grafikę
+	texture_rect.texture = texture2
+	
+	# kolejny etap ładowania (3/9)
+	await get_tree().create_timer(0.6).timeout
+	var texture3 = preload("res://assets/kopiowanie3.png")
+	texture_rect.texture = texture3
+	
+	# kolejny etap ładowania (4/9)
+	await get_tree().create_timer(0.6).timeout
+	var texture4 = preload("res://assets/kopiowanie4.png")
+	texture_rect.texture = texture4
+	
+	# kolejny etap ładowania (5/9)
+	await get_tree().create_timer(0.6).timeout
+	var texture5 = preload("res://assets/kopiowanie5.png")
+	texture_rect.texture = texture5
+	
+	# kolejny etap ładowania (6/9)
+	await get_tree().create_timer(0.6).timeout
+	var texture6 = preload("res://assets/kopiowanie6.png")
+	texture_rect.texture = texture6
+	
+	# kolejny etap ładowania (7/9)
+	await get_tree().create_timer(0.6).timeout
+	var texture7 = preload("res://assets/kopiowanie7.png")
+	texture_rect.texture = texture7
+	
+	# kolejny etap ładowania (8/9)
+	await get_tree().create_timer(0.6).timeout
+	var texture8 = preload("res://assets/kopiowanie8.png")
+	texture_rect.texture = texture8
+	
+	# kolejny etap ładowania (9/9)
+	await get_tree().create_timer(0.6).timeout
+	var texture9 = preload("res://assets/kopiowanie9.png")
+	texture_rect.texture = texture9
+	
+	#kończymy ładowanie przechodząc do sceny końcowej
+	await get_tree().create_timer(0.6).timeout
+	# Przełącz do sceny minigame_copying_files_end.tscn
+	get_tree().change_scene_to_file("res://minigame_copying_files_end.tscn")
+	
+
+
---
+++ scenes/minigames/copying-files/minigame_copying_files_end.gd
@@ -0,0 +1,5 @@
+extends Node2D
+
+func _on_end_button_down():
+	# Wyślij potwierdzenie wykonania taska
+	print("Task sucessfull")
