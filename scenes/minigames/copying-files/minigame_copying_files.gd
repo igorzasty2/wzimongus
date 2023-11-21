2dc31666fd7796eb4666eeb321c61dfbd8b2a857
Patryk Matuszewski <patrykmatuszewski02@icloud.com>
2023-11-21 13:26:52 +0100

    path error fixing
    changes of load path in scenes and script

--- scenes/minigames/copying-files/minigame_copying_files.gd
+++ scenes/minigames/copying-files/minigame_copying_files.gd
@@ -3,7 +3,7 @@ extends Button
 
 func _on_confirm_button_down():
 	# Załaduj grafikę
-	var texture = preload("res://assets/kopiowanie1.png")
+	var texture = preload("res://scenes/minigames/copying-files/assets/kopiowanie1.png")
 
 	# Ustaw grafikę w węźle tekstury
 	var texture_rect = TextureRect.new()
@@ -18,49 +18,49 @@ func _on_confirm_button_down():
 	# Oczekaj przez 0.5 sekundy
 	await get_tree().create_timer(0.6).timeout
 	# Załaduj drugą grafikę
-	var texture2 = preload("res://assets/kopiowanie2.png")
+	var texture2 = preload("res://scenes/minigames/copying-files/assets/kopiowanie2.png")
 	# Zmień teksturę na drugą grafikę
 	texture_rect.texture = texture2
 	
 	# kolejny etap ładowania (3/9)
 	await get_tree().create_timer(0.6).timeout
-	var texture3 = preload("res://assets/kopiowanie3.png")
+	var texture3 = preload("res://scenes/minigames/copying-files/assets/kopiowanie3.png")
 	texture_rect.texture = texture3
 	
 	# kolejny etap ładowania (4/9)
 	await get_tree().create_timer(0.6).timeout
-	var texture4 = preload("res://assets/kopiowanie4.png")
+	var texture4 = preload("res://scenes/minigames/copying-files/assets/kopiowanie4.png")
 	texture_rect.texture = texture4
 	
 	# kolejny etap ładowania (5/9)
 	await get_tree().create_timer(0.6).timeout
-	var texture5 = preload("res://assets/kopiowanie5.png")
+	var texture5 = preload("res://scenes/minigames/copying-files/assets/kopiowanie5.png")
 	texture_rect.texture = texture5
 	
 	# kolejny etap ładowania (6/9)
 	await get_tree().create_timer(0.6).timeout
-	var texture6 = preload("res://assets/kopiowanie6.png")
+	var texture6 = preload("res://scenes/minigames/copying-files/assets/kopiowanie6.png")
 	texture_rect.texture = texture6
 	
 	# kolejny etap ładowania (7/9)
 	await get_tree().create_timer(0.6).timeout
-	var texture7 = preload("res://assets/kopiowanie7.png")
+	var texture7 = preload("res://scenes/minigames/copying-files/assets/kopiowanie7.png")
 	texture_rect.texture = texture7
 	
 	# kolejny etap ładowania (8/9)
 	await get_tree().create_timer(0.6).timeout
-	var texture8 = preload("res://assets/kopiowanie8.png")
+	var texture8 = preload("res://scenes/minigames/copying-files/assets/kopiowanie8.png")
 	texture_rect.texture = texture8
 	
 	# kolejny etap ładowania (9/9)
 	await get_tree().create_timer(0.6).timeout
-	var texture9 = preload("res://assets/kopiowanie9.png")
+	var texture9 = preload("res://scenes/minigames/copying-files/assets/kopiowanie9.png")
 	texture_rect.texture = texture9
 	
 	#kończymy ładowanie przechodząc do sceny końcowej
 	await get_tree().create_timer(0.6).timeout
 	# Przełącz do sceny minigame_copying_files_end.tscn
-	get_tree().change_scene_to_file("res://minigame_copying_files_end.tscn")
+	get_tree().change_scene_to_file("res://scenes/minigames/copying-files/minigame_copying_files_end.tscn")
 	
 
 
