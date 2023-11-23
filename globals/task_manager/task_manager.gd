4f7166465f2489a889813f827693633e0620a773
Dawid Maliszewski <dawid@thedavesky.com>
2023-11-23 16:02:11 +0100

    Update task assignment logic
    Co-authored-by: Ilya Pauliuk <ilyapavluk24@gmail.com>

--- globals/task_manager/task_manager.gd
+++ globals/task_manager/task_manager.gd
@@ -1,51 +1,56 @@
 extends Node
-# TODO: Needs to load minigames as a list here so that we can iterate through them
-var minigames = []
 
-# Task id is being assigned to every task. Should be incremented every time a task is assigned.
-var task_id = 0
+# TODO: Potrzebuje tutaj załadować minigry jako listę, aby móc przez nie iterować
+var minigames = {}
 
-# Stores all the tasks in a server specific list.
-var tasks_server = []
-# Stores current player's tasks.
-var tasks_player = []
+# Task ID bieżącego zadania.
+var current_task_id = 0
+
+
+# Przechowuje wszystkie zadania w liście specyficznej dla serwera.
+var tasks_server = {}
+# Przechowuje zadania bieżącego gracza.
+var tasks_player = {}
 
 
 @rpc("any_peer", "call_local")
-func mark_task_as_done(task_id, player_id):
-	if multiplayer.is_server():
-		# Removes task from task_list_server by task id reference.
-		var task_index = _find_task_list_index_by_task_id(tasks_server, task_id)
-		if task_index != null:
-			tasks_server.remove_at(task_index)	
-		
-	
-	if player_id == multiplayer.get_unique_id():
-		# Removes task from current player's task list.
-		var task_index = _find_task_list_index_by_task_id(tasks_player, task_id)
-		if task_index != null:
-			tasks_player.remove_at(task_index)
+func mark_task_as_done_player():
+	# Usuwa zadanie z listy zadań bieżącego gracza.
+	var player_id = multiplayer.get_unique_id()
+	tasks_player.erase(current_task_id)
+
+	mark_task_as_done_server.rpc_id(1, player_id, current_task_id)
 
 
-func _find_task_list_index_by_task_id(task_list, task_id):
-	for i in range(len(task_list)):
-		if task_list[i].task_id == task_id:
-			return i
+@rpc("call_remote", "any_peer")
+func mark_task_as_done_server(player_id, task_id):
+	# Usuwa zadanie z listy tasków na serwerze.
+	tasks_server[player_id].erase(task_id)
+
+	if tasks_server.is_empty():
+		# TODO: zaznacz że crewmate wygrali jeżeli nie ma już tasków do zrobienia
+		pass
 
 
 @rpc("authority", "call_local")
-func assign_tasks_server():
+func assign_tasks_server(task_amount):
 	# TODO: for this function to work first minigames list should be created.
 	if multiplayer.is_server():
 		if tasks_server.is_empty():
-			# TODO: populate tasks_server here
-			# my proposition is to generate task player by player
-			
-			# TODO: send task list to the corresponding player
-			# assign_tasks_player.rpc_id(player_id, tasks)
-			pass
+			var _id_counter = 0
+			for i in MultiplayerManager.players:
+				var tasks_dict = {}
 
+				for task_number in range(task_amount):
+					# TODO: refactor to generate random task with no repetitions.
+					tasks_dict[_id_counter] = minigames[randi() % minigames.size()]
+				
+				tasks_server[i] = tasks_dict
+				assign_tasks_player.rpc_id(1, tasks_dict)
+				
+				_id_counter += 1
 
 @rpc("authority", "call_remote")
 func assign_tasks_player(tasks):
+	
 	tasks_player.append_array(tasks)
