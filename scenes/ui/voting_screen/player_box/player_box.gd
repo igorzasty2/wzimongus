dfeeef5b88b4965e4b1c85c3efa1417da85e6329
Kacper Pełka <kacper.pelka03@gmail.com>
2023-12-05 04:38:31 +0100

    Add display animation for voted avatars

--- scenes/ui/voting_screen/player_box/player_box.gd
+++ scenes/ui/voting_screen/player_box/player_box.gd
@@ -7,6 +7,7 @@ extends Control
 signal player_voted
 
 var player_key
+var display_tween
 
 var voted_by_scene = preload("res://scenes/ui/voting_screen/voted_by/voted_by.tscn")
 
@@ -15,22 +16,23 @@ func init(username: String, player_key: int, voted_by):
 	self.player_key = player_key
 
 	for vote in voted_by:
+
 		var voted_by_instance = voted_by_scene.instantiate()
+		voted_by_instance.modulate.a = 0;
+		display_tween = get_tree().create_tween()
+		display_tween.tween_property(voted_by_instance, "modulate:a", 1, 0.25)
+		
 		voted_by_container.add_child(voted_by_instance)
 		
 
 	if GameManager.get_current_player_key("died"):
 		self.disconnect("player_voted", _on_button_pressed)
-	
-
 
 func _on_button_pressed():
 	if GameManager.get_current_player_key("voted"):
 		return
 
 	decision.visible = true
-
-
 
 func _on_decision_no_pressed():
 	decision.visible = false
