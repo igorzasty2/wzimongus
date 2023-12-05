2b92d4e4b4d90704860d492ae9d0ec1b3f4c9320
Sebastian Matejak <sebastian.mat.2201@gmail.com>
2023-12-05 08:33:44 +0100

    Moved use button and minigame viewport to map
    - Added camera from another branch to test how it works with use button and viewport
    - Moved use button and viewport to map as camera's children to make them work with camera

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -11,8 +11,11 @@ const SPEED = 300.0
 var minigame: PackedScene
 var minigame_instance:Node2D
 
+@onready var minigame_container = get_parent().get_parent().get_node("Camera2D").get_node("MinigameContainer")
+@onready var use_button = get_parent().get_parent().get_node("Camera2D").get_node("UseButton")
 @onready var input = $InputSynchronizer
 @onready var animation_tree = $Skins/AltAnimationTree
+@onready var camera = get_parent().get_parent().get_node("Camera2D")
 
 
 func _ready():
@@ -28,10 +31,15 @@ func _ready():
 	$UsernameLabel.text = username
 	animation_tree.active = true
 	last_direction_x = 1
+	use_button.pressed.connect(_on_use_button_pressed)
 
 
 func _process(_delta):
 	update_animation_parameters()
+	
+	if name.to_int() == multiplayer.get_unique_id():
+		var target_position = position
+		camera.position = camera.position.lerp(target_position, 5 * _delta)
 
 
 func _physics_process(_delta):
@@ -74,19 +82,20 @@ func _on_input_state_changed(state: bool):
 func show_use_button(id, minigame):
 	if id == multiplayer.get_unique_id():
 		self.minigame = minigame
-		$UseButton.visible = true
-		$UseButton.disabled = false
+		use_button.visible = true
+		use_button.disabled = false
 
 
 func hide_use_button(id):
 	if id == multiplayer.get_unique_id():
 		minigame = null
-		$UseButton.visible = false
-		$UseButton.disabled = true
+		use_button.visible = false
+		use_button.disabled = true
 
 
 func _on_use_button_pressed():
-	summon_window()
+	if minigame != null:
+		summon_window()
 
 
 func _input(event):
@@ -94,18 +103,19 @@ func _input(event):
 		summon_window()
 
 func summon_window():
-	$MinigameContainer.visible = true
-	$MinigameContainer/MinigameViewport.add_child(minigame.instantiate())
-	minigame_instance = $MinigameContainer/MinigameViewport.get_child(0)
-	var x_scale = $MinigameContainer/MinigameViewport.size.x / get_viewport_rect().size.x
-	var y_scale = $MinigameContainer/MinigameViewport.size.y / get_viewport_rect().size.y
+	minigame_container.visible = true
+	var minigame_viewport = minigame_container.get_node("MinigameViewport")
+	minigame_viewport.add_child(minigame.instantiate())
+	minigame_instance = minigame_viewport.get_child(0)
+	var x_scale = minigame_viewport.size.x / get_viewport_rect().size.x
+	var y_scale = minigame_viewport.size.y / get_viewport_rect().size.y
 	minigame_instance.scale = Vector2(x_scale, y_scale)
-	$UseButton.visible = false
-	$UseButton.disabled = true
+	use_button.visible = false
+	use_button.disabled = true
 	MultiplayerManager.set_input_state(false)
 	minigame_instance.minigame_end.connect(end_minigame)
 
 func end_minigame():
 	minigame_instance.queue_free()
-	$MinigameContainer.visible = false
+	minigame_container.visible = false
 	MultiplayerManager.set_input_state(true)
