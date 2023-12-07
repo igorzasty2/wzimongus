18f6e520d47333dea2452b260e2352939c15f663
Kamil Marszałek <108593228+KamilMarshal@users.noreply.github.com>
2023-12-08 00:57:52 +0100

    Implementing camera shaking method
    camera.shake(time,shake_amount)

--- scenes/map/camera.gd
+++ scenes/map/camera.gd
@@ -6,8 +6,30 @@ var camera_speed = 5.0
 # Referencja do postaci śledzonego gracza.
 var player = CharacterBody2D
 
+var shake_amount : float = 0
+var default_offset : Vector2 = offset
+
+@onready var timer : Timer = $Timer
+@onready var tween : Tween = create_tween()
+
+func _ready():
+	set_process(true)
+	randomize()
 
 func _process(delta):
 	if player:
 		# Interpolacja pozycji kamery w kierunku pozycji gracza.
 		position = position.lerp(player.position, camera_speed * delta)
+		
+	offset = Vector2(randf_range(-1,1) * shake_amount,randf_range(-1,1) * shake_amount)
+
+func shake(time : float, amount : float):
+	timer.wait_time = time
+	shake_amount = amount
+	set_process(true)
+	timer.start()
+	
+
+func _on_timer_timeout() -> void:
+	set_process(false)
+	tween.interpolate_value(self, "offset", 1, 1, Tween.TRANS_LINEAR, Tween.EASE_IN)
