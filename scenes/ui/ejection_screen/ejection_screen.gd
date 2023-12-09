567ebe1673e14b0ba0fa2e60423ecb504d7a7d56
Valcast <kacper.pelka03@gmail.com>
2023-12-09 04:24:54 +0100

    Code Refactor and bug fixing

--- scenes/ui/ejection_screen/ejection_screen.gd
+++ scenes/ui/ejection_screen/ejection_screen.gd
@@ -3,32 +3,59 @@ extends Control
 @onready var ejection_message = get_node("%EjectionMessage")
 @onready var votes = GameManager.get_votes()
 
+@export var NEXT_ROUND_TIME = 5
+@onready var next_round_timer = Timer.new()
+
+
 func _ready():
-	var most_voted_player_id
+	var most_voted_player_id = get_most_voted_player_id()
+	var message = get_ejection_message(most_voted_player_id)
+	ejection_message.text = message
+
+	#NEXT ROUND TIMER
+	add_child(next_round_timer)
+	next_round_timer.autostart = true
+	next_round_timer.one_shot = true
+	next_round_timer.connect("timeout", _on_next_round_timer_timeout)
+	next_round_timer.start(NEXT_ROUND_TIME)
+
+	
+func get_most_voted_player_id():
+	var most_voted_players = []
 	var max_vote = 0
-	var prev_max_vote = 0
-	var is_tie = false
 
 	for vote_key in votes.keys():
 		var votes_count = votes[vote_key].size()
 		if votes_count > max_vote:
 			max_vote = votes_count
-			most_voted_player_id = vote_key
-			is_tie = false
-		elif votes_count == max_vote and votes_count != prev_max_vote:
-			is_tie = true
+			most_voted_players = [vote_key]
+		elif votes_count == max_vote:
+			most_voted_players.append(vote_key)
 
-		prev_max_vote = votes_count
-
-
-	if is_tie:
-		most_voted_player_id = ""
-	
-	if str(max_vote) == "":
-		ejection_message.text = "No one was ejected."
-	elif(GameManager.get_registered_player_key(most_voted_player_id, 'impostor')):
-		ejection_message.text = GameManager.get_registered_player_key(most_voted_player_id, 'username') + " was ejected."
+	if most_voted_players.size() > 1 || most_voted_players.size() == 0:
+		return null
 	else:
-		ejection_message.text = GameManager.get_registered_player_key(most_voted_player_id, 'username') + " was not an impostor."
-		
+		return most_voted_players[0]
+
+
+func get_ejection_message(player_id):
+	if player_id == null:
+		return "[center]No one was ejected.[/center]"
+	elif GameManager.get_registered_player_key(player_id, 'impostor'):
+		#TODO: GameManager.set_registered_player_key(player_id, 'alive', false)
+		return "[center]" + GameManager.get_registered_player_key(player_id, 'username') + " was ejected.[/center]"
+	else:
+		#TODO: GameManager.set_registered_player_key(player_id, 'alive', false)
+		return "[center]" + GameManager.get_registered_player_key(player_id, 'username') + " was not an impostor.[/center]"
+
+
+func _on_next_round_timer_timeout():
+	self.queue_free()
+	GameManager.set_input_status(true)
+	GameManager.set_pause_status(false)
+	self.get_parent().get_node("%report_button").show()
+	GameManager.set_player_key("voted", false)
+
+	#! Nie powinno usuwać tutaj, tylko w GameManagerze, podczas inicjalizacji następnej rundy
+	GameManager._votes = {}
 
--- scenes/ui/voting_screen/voting_screen.gd
+++ scenes/ui/voting_screen/voting_screen.gd
@@ -5,28 +5,28 @@ extends Control
 @onready var skip_decision = get_node("%Decision")
 @onready var skip_button = get_node("%SkipButton")
 
-@export var VOTING_TIME = 60
+@export var VOTING_TIME = 10
 @onready var voting_timer = Timer.new()
 
 @export var EJECT_PLAYER_TIME = 5
 @onready var eject_player_timer = Timer.new()
 
 var player_box = preload("res://scenes/ui/voting_screen/player_box/player_box.tscn")
+var ejection_screen = preload("res://scenes/ui/ejection_screen/ejection_screen.tscn")
 
 var time = 0
-var secs
 
 func _ready():
 	_render_player_boxes()
 
-	#End voting timer
+	#END VOTING TIMER
 	add_child(voting_timer)
 	voting_timer.autostart = true
 	voting_timer.one_shot = true
 	voting_timer.connect("timeout", _on_end_voting_timer_timeout)
 	voting_timer.start(VOTING_TIME)
 
-	#Eject player timer
+	#EJECT PLAYER TIMER
 	add_child(eject_player_timer)
 	eject_player_timer.connect("timeout", _on_eject_player_timer_timeout)
 
@@ -34,24 +34,22 @@ func _ready():
 func _process(delta):
 	if time < VOTING_TIME:
 		time += delta
-		secs = VOTING_TIME - fmod(time, VOTING_TIME)
-		var time_passed = "%02d" % (secs)
-		end_vote_text.text = "Voting ends in " + time_passed + " seconds"
+		var time_remaining = VOTING_TIME - time
+		end_vote_text.text = "Voting ends in %02d seconds" % time_remaining
 
 
 func _on_player_voted(voted_player_key):
 	skip_button.disabled = true
 	GameManager.set_player_key("voted", true)
-	add_player_vote.rpc(voted_player_key, multiplayer.get_unique_id())
+	_add_player_vote.rpc(voted_player_key, multiplayer.get_unique_id())
 
 
 @rpc("call_local", "any_peer")
-func add_player_vote(player_key, voted_by):
+func _add_player_vote(player_key, voted_by):
 	GameManager.add_vote(player_key, voted_by)
 
 
 func _on_skip_button_pressed():
-
 	if GameManager.get_current_player_key("voted_for"):
 		return
 
@@ -67,9 +65,7 @@ func _on_decision_no_pressed():
 	skip_decision.visible = false
 
 
-
 func _render_player_boxes():
-
 	var registered_players = GameManager.get_registered_players()
 
 	var votes = GameManager.get_votes()
@@ -84,10 +80,10 @@ func _render_player_boxes():
 
 
 func _on_end_voting_timer_timeout():
-	if GameManager.get_current_player_key("voted"):
-		GameManager.set_player_key("voted_for", true)
+	if !GameManager.get_current_player_key("voted"):
+		GameManager.set_player_key("voted", true)
 
-	end_vote_text.text = "Voting has ended"
+	end_vote_text.text = "[center]Voting has ended[/center]"
 
 	for child in players.get_children():
 		child.queue_free()
@@ -100,7 +96,8 @@ func _on_end_voting_timer_timeout():
 func _on_eject_player_timer_timeout():
 	_change_scene_to_ejection_screen.rpc()
 
+
 @rpc("call_local", "any_peer")
 func _change_scene_to_ejection_screen():
-	get_tree().change_scene_to_file("res://scenes/ui/ejection_screen/ejection_screen.tscn")
+	self.get_parent().add_child(ejection_screen.instantiate())
 	self.queue_free()
