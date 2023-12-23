58fd39df10722cda2a99f963f9c3e374f6141641
Dawid Maliszewski <dawid@thedavesky.com>
2023-12-23 15:49:05 +0100

    Redesign join menu

--- scenes/ui/join_menu/join_menu.gd
+++ scenes/ui/join_menu/join_menu.gd
@@ -3,7 +3,7 @@ extends Control
 @onready var username_input = $InputsContainer/UsernameContainer/UsernameInput
 @onready var address_input = $InputsContainer/AddressContainer/AddressInput
 @onready var port_input = $InputsContainer/PortContainer/PortInput
-@onready var server_list = $ServerList
+@onready var server_list = $ServerPanel/Table/ServerList
 
 func _on_join_button_button_down():
 	GameManager.set_player_key("username", username_input.text)
@@ -14,21 +14,19 @@ func _on_join_button_button_down():
 
 
 func _on_server_listener_new_server(server_info):
-	print(server_info)
 	var server_node = preload("res://scenes/ui/join_menu/server_node/server_node.tscn").instantiate()
-	server_node.init(server_info)
+	server_node.init.call_deferred(server_info)
 	server_node.connect("server_selected", _on_server_selected)
 	server_list.add_child(server_node)
 
 
 func _on_server_listener_remove_server(server_ip):
-	print(server_ip)
 	for server_node in server_list.get_children():
-		if server_node.text.find(server_ip) != -1:
+		if server_node.get_server_ip() == server_ip:
 			server_node.disconnect("server_selected", _on_server_selected)
 			server_list.remove_child(server_node)
 			server_node.queue_free()
-			break
+
 
 func _on_server_selected(server_info):
 	address_input.text = server_info.ip
--- scenes/ui/join_menu/server_node/server_node.gd
+++ scenes/ui/join_menu/server_node/server_node.gd
@@ -1,22 +1,21 @@
-extends HBoxContainer
-
-var _server_info
+extends MarginContainer
 
 signal server_selected(server_info)
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+var _server_info
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+@onready var lobby_name = $EntryContainer/InfoContainer/LobbyName
+@onready var server_address = $EntryContainer/InfoContainer/ServerAddress
 
 func init(server_info):
 	_server_info = server_info
-	$LobbyName.text = server_info.name
-	$ServerAddress.text = server_info.ip + ":" + str(server_info.port)
+	lobby_name.text = server_info.name
+	server_address.text = server_info.ip + ":" + str(server_info.port)
+
+
+func get_server_ip():
+	return _server_info.ip
+
 
 func _on_button_pressed():
 	server_selected.emit(_server_info)
