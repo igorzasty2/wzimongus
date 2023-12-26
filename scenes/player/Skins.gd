49a041b0e1384a894da4019e685611ebfb650111
Kamil Marszałek <108593228+KamilMarshal@users.noreply.github.com>
2023-12-26 19:22:22 +0100

    fixed skin changing and move it to the function skin_change()

--- scenes/player/Skins.gd
+++ scenes/player/Skins.gd
@@ -1,10 +1,12 @@
 extends Node2D
 
 func _ready():
+	skin_change()
 	
+func skin_change():	
 	var skin: Image	
 	var foto: ImageTexture
-	match GameManager.get_current_player_key("skin"):
+	match GameManager.get_registered_player_key(get_parent().name.to_int(),"skin"):
 		0:
 			skin = Image.load_from_file("res://scenes/player/assets/skins/alt_spritesheet.png")
 		1:
