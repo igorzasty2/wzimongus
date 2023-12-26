5e6299353a53c71fa76248bdd26dfb1fb3c9ee14
Kamil Marszałek <108593228+KamilMarshal@users.noreply.github.com>
2023-12-26 19:51:11 +0100

    changing select style spritesheet and creating space for upcoming skins

--- scenes/player/Skins.gd
+++ scenes/player/Skins.gd
@@ -11,6 +11,23 @@ func skin_change():
 			skin = Image.load_from_file("res://scenes/player/assets/skins/alt_spritesheet.png")
 		1:
 			skin = Image.load_from_file("res://scenes/player/assets/skins/nerd_spritesheet.png")
-			
+	# Poniższe do uzupełnienia!!!!
+		2:
+			skin = Image.load_from_file("res://scenes/player/assets/skins/alt_spritesheet.png")
+		3:
+			skin = Image.load_from_file("res://scenes/player/assets/skins/nerd_spritesheet.png")
+		4:
+			skin = Image.load_from_file("res://scenes/player/assets/skins/alt_spritesheet.png")
+		5:
+			skin = Image.load_from_file("res://scenes/player/assets/skins/nerd_spritesheet.png")
+		6:
+			skin = Image.load_from_file("res://scenes/player/assets/skins/alt_spritesheet.png")
+		7:
+			skin = Image.load_from_file("res://scenes/player/assets/skins/nerd_spritesheet.png")
+		8:
+			skin = Image.load_from_file("res://scenes/player/assets/skins/alt_spritesheet.png")
+		9:
+			skin = Image.load_from_file("res://scenes/player/assets/skins/nerd_spritesheet.png")
+
 	foto = ImageTexture.create_from_image(skin)
 	$AltSprite.texture = foto
