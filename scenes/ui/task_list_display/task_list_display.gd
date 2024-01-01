383c62adf0ddadc59bfbb383eae453a4b3e43021
iljuhenson <ilyapavluk24@gmail.com>
2024-01-01 11:08:03 +0100

    Add task names to the display task list based on the minigame scene English name

--- scenes/ui/task_list_display/task_list_display.gd
+++ scenes/ui/task_list_display/task_list_display.gd
@@ -10,7 +10,7 @@ func _update_task_list():
 	var tasks_string = ""
 	
 	for i in TaskManager.current_player_tasks:
-		tasks_string += str(TaskManager.current_player_tasks[i].minigame_scene)
+		tasks_string += str(" ".join(TaskManager.current_player_tasks[i].minigame_scene.resource_path.split('/')[-1].split('.')[0].split('_')).capitalize())
 		tasks_string += "\n"
 	
 	_task_list_node.text = tasks_string
