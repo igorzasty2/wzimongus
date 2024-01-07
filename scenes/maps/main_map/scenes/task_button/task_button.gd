b4aa066bb1a9fb631fa55a741e545732b308dca3
SGGWAmongUs <149107548+SGGWAmongUs@users.noreply.github.com>
2024-01-07 16:25:01 +0100

    Update task_button.gd
    Unified condition symbols

--- scenes/maps/main_map/scenes/task_button/task_button.gd
+++ scenes/maps/main_map/scenes/task_button/task_button.gd
@@ -40,7 +40,7 @@ func _ready():
 
 
 func _on_body_entered(body):
-	if body.name.to_int() == multiplayer.get_unique_id() and not disabled && !body.is_in_vent:
+	if body.name.to_int() == multiplayer.get_unique_id() && !disabled && !body.is_in_vent:
 		_is_player_inside = true
 		sprite_node.material.set_shader_parameter('line_color', _in_range_task_color)
 		minigame_menu.show_use_button(minigame_scene)
@@ -48,7 +48,7 @@ func _on_body_entered(body):
 
 
 func _on_body_exited(body):
-	if body.name.to_int() == multiplayer.get_unique_id() and not disabled && !body.is_in_vent:
+	if body.name.to_int() == multiplayer.get_unique_id() && !disabled && !body.is_in_vent:
 		_is_player_inside = false
 		sprite_node.material.set_shader_parameter('line_color', _out_of_range_task_color)
 		minigame_menu.hide_use_button()
