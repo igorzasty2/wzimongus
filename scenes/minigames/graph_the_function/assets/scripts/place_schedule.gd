fc4725322b00f71205ca85fba48b8ef2367db03c
Mateusz Ługowski <mateusz.lugowski@onet.eu>
2024-01-08 22:39:26 +0100

    Minigame update
    Added sinus pattern f(x) = sin(x-p)+q, text center

--- scenes/minigames/graph_the_function/assets/scripts/place_schedule.gd
+++ scenes/minigames/graph_the_function/assets/scripts/place_schedule.gd
@@ -11,7 +11,7 @@ func _ready():
 		y = randi() % 9 - 4
 	x_func = x
 	y_func = y
-	var textToInput = "Skonstruuj wykres sinusa przy użyciu podanego wzoru sin(x"
+	var textToInput = "[center]Skonstruuj wykres sinusa przy użyciu podanego wzoru sin(x"
 	if x > 0:
 		textToInput += " - "
 	else:
@@ -21,7 +21,7 @@ func _ready():
 		textToInput += " + "
 	else:
 		textToInput += " - "
-	textToInput += str(abs(y))
+	textToInput += str(abs(y)) + "[/center]"
 	$TextEdit.text = textToInput
 
 
@@ -34,6 +34,16 @@ func _on_v_slider_drag_ended(value_changed):
 	$VText.text = str($VSlider.value)
 	var new_position = Vector2(632 + $HSlider.value * 41, 410 - $VSlider.value * 40)
 	$Pngegg.position = new_position
+	$TextEdit2.text = "[center]f(x) = sin(x"
+	if($HSlider.value < 0):
+		$TextEdit2.text += " + " + str(abs($HSlider.value))
+	else:
+		$TextEdit2.text += " - " + str(abs($HSlider.value))
+	if($VSlider.value < 0):
+		$TextEdit2.text += ") - "+str(abs($VSlider.value))+"[/center]"
+	else:
+		$TextEdit2.text += ") + " + str(abs($VSlider.value))
+		
 	check_if_all_coincidence()
 
 
@@ -41,4 +51,13 @@ func _on_h_slider_drag_ended(value_changed):
 	$HText.text = str($HSlider.value)
 	var new_position = Vector2(632 + $HSlider.value * 41, 410 - $VSlider.value * 40)
 	$Pngegg.position = new_position
+	$TextEdit2.text = "[center]f(x) = sin(x"
+	if($HSlider.value < 0):
+		$TextEdit2.text += " + " + str(abs($HSlider.value))
+	else:
+		$TextEdit2.text += " - " + str(abs($HSlider.value))
+	if($VSlider.value < 0):
+		$TextEdit2.text += ") - "+str(abs($VSlider.value))+"[/center]"
+	else:
+		$TextEdit2.text += ") + " + str(abs($VSlider.value))
 	check_if_all_coincidence()
