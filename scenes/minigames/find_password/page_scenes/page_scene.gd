bd503c083f5da03f407ec8a83d6dd46a5dc8f132
Kacper Pełka <kacper.pelka03@gmail.com>
2024-01-10 03:05:34 +0100

    Change exit from ESC to Close Button

--- scenes/minigames/find_password/page_scenes/page_scene.gd
+++ scenes/minigames/find_password/page_scenes/page_scene.gd
@@ -1,11 +1,7 @@
-extends Sprite2D
-
-func _input(event):
-	if event.is_action_pressed("pause_menu"):
-		visible = false
-		
+extends Sprite2D		
 
 func init(password):
 	$PageText.text = password
 
-
+func _on_button_pressed():
+	visible = false
