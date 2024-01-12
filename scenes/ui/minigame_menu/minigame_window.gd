d3010b3dd36254fc3902f7ddfc064ca39150bddd
Sebastian Matejak <sebastian.mat.2201@gmail.com>
2024-01-12 19:47:54 +0100

    Added documentation to minigame window

--- scenes/ui/minigame_menu/minigame_window.gd
+++ scenes/ui/minigame_menu/minigame_window.gd
@@ -1,15 +1,20 @@
+class_name MinigameWindow
 extends CanvasLayer
-
+## Klasa ta reprezentuje okno minigry
 
 var _minigame: PackedScene
 var _minigame_instance: Node
+## Inforuje o stanie przycisku aktywacji minigry
 var use_button_disabled: bool = true
 
+## Referencja do Node'a w drzewie reprezentującego całość okna minigry
 @onready var minigame_container = $MinigameContainer
+## Referencja do subviewport'a wyświetlającego minigrę
 @onready var subviewport = minigame_container.get_node("SubviewportContainer/MinigameViewport")
+## Referencja do przycisku zamykającego okno minigry
 @onready var close_button: TextureButton = minigame_container.get_node("CloseButton")
 
-# Zmienne do obsługi interface gracza
+## Zmienne do obsługi interface gracza
 @onready var user_interface = get_parent().get_node("UserInterface")
 signal use_button_active(is_active:bool)
 
@@ -18,20 +23,20 @@ func _ready():
 	close_button.pressed.connect(close_minigame)
 	use_button_active.connect(user_interface.toggle_button_active)
 
-
+## Aktywuje przycisk aktywacji minigry
 func show_use_button(minigame):
 	_minigame = minigame
 	emit_signal("use_button_active", "InteractButton", true)
 	use_button_disabled = false
 
-
+## Wyłącza przycisk aktywacji minigry
 func hide_use_button():
 	_minigame = null
 	emit_signal("use_button_active", "InteractButton", false)
 	use_button_disabled = false
 
 
-# connect in user interface to this
+## W chwili naciśnięcia przycisku aktywuje funkcję wyświetlającą okno minigry
 func _on_use_button_pressed():
 	if _minigame == null:
 		return
@@ -42,6 +47,7 @@ func _on_use_button_pressed():
 	summon_window()
 
 
+## Daje możliwość aktywacji minigry przez wciśnięcie przycisku na klawiaturze
 func _input(event):
 	if event.is_action_pressed("pause_menu"):
 		if !visible:
@@ -68,7 +74,7 @@ func _input(event):
 
 		summon_window()
 
-
+## Pokazuje okno minigry
 func summon_window():
 	show()
 
@@ -84,6 +90,7 @@ func summon_window():
 		for camera in get_tree().get_nodes_in_group("cameras"):
 			camera.change_light_visibility()
 
+## Ukrywa okno minigry w chwili jej ukończenia i odznacza zadanie za zakończone
 func end_minigame():
 	_minigame_instance.queue_free()
 
@@ -92,7 +99,7 @@ func end_minigame():
 
 	TaskManager.mark_task_as_complete()
 
-
+## Ukrywa okno minigry pomimo nie ukończenia jej w chwili wciśnięcia przycisku
 func close_minigame():
 	if _minigame_instance != null:
 		_minigame_instance.queue_free()
