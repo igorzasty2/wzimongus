8cccdd7a6e791c374a02ed80a793adf3668831fb
Dawid Maliszewski <dawid@thedavesky.com>
2024-01-14 01:45:15 +0100

    Bump Godot to 4.2.x

--- scenes/menu/host_menu/host_menu.gd
+++ scenes/menu/host_menu/host_menu.gd
@@ -1,16 +1,19 @@
+## Klasa odpowiedzialna za obsługę menu hosta.
+class_name HostMenu
 extends Control
 
-@onready var lobby_name_input = $InputsContainer/LobbyNameContainer/LobbyNameInput
-@onready var username_input = $InputsContainer/UsernameContainer/UsernameInput
-@onready var port_input = $InputsContainer/PortContainer/PortInput
+
+@onready var _lobby_name_input = $InputsContainer/LobbyNameContainer/LobbyNameInput
+@onready var _username_input = $InputsContainer/UsernameContainer/UsernameInput
+@onready var _port_input = $InputsContainer/PortContainer/PortInput
 
 
 func _on_host_button_pressed():
-	GameManagerSingleton.set_current_player_value("username", username_input.text)
+	GameManagerSingleton.set_current_player_value("username", _username_input.text)
+
+	GameManagerSingleton.create_lobby.call_deferred(_lobby_name_input.text, _port_input.text.to_int())
 
 	get_tree().change_scene_to_file("res://scenes/game/game.tscn")
-
-	GameManagerSingleton.create_lobby.call_deferred(lobby_name_input.text, port_input.text.to_int())
 
 
 func _on_back_button_pressed():
--- scenes/menu/join_menu/join_menu.gd
+++ scenes/menu/join_menu/join_menu.gd
@@ -1,36 +1,40 @@
+## Klasa odpowiedzialna za menu wyboru serwera.
+class_name JoinMenu
 extends Control
 
-@onready var username_input = $InputsContainer/UsernameContainer/UsernameInput
-@onready var address_input = $InputsContainer/AddressContainer/AddressInput
-@onready var port_input = $InputsContainer/PortContainer/PortInput
-@onready var server_list = $ServerPanel/Table/ServerListScroll/ServerList
+
+@onready var _username_input = $InputsContainer/UsernameContainer/UsernameInput
+@onready var _address_input = $InputsContainer/AddressContainer/AddressInput
+@onready var _port_input = $InputsContainer/PortContainer/PortInput
+@onready var _server_list = $ServerPanel/Table/ServerListScroll/ServerList
+
 
 func _on_join_button_pressed():
-	GameManagerSingleton.set_current_player_value("username", username_input.text)
+	GameManagerSingleton.set_current_player_value("username", _username_input.text)
+
+	GameManagerSingleton.join_lobby.call_deferred(_address_input.text, _port_input.text.to_int())
 
 	get_tree().change_scene_to_file("res://scenes/game/game.tscn")
-
-	GameManagerSingleton.join_lobby.call_deferred(address_input.text, port_input.text.to_int())
 
 
 func _on_server_listener_new_server(server_info):
 	var server_node = preload("res://scenes/menu/join_menu/server_node/server_node.tscn").instantiate()
 	server_node.init.call_deferred(server_info)
 	server_node.connect("server_selected", _on_server_selected)
-	server_list.add_child(server_node)
+	_server_list.add_child(server_node)
 
 
 func _on_server_listener_remove_server(server_ip):
-	for server_node in server_list.get_children():
+	for server_node in _server_list.get_children():
 		if server_node.get_server_ip() == server_ip:
 			server_node.disconnect("server_selected", _on_server_selected)
-			server_list.remove_child(server_node)
+			_server_list.remove_child(server_node)
 			server_node.queue_free()
 
 
 func _on_server_selected(server_info):
-	address_input.text = server_info["ip"]
-	port_input.text = str(server_info["port"])
+	_address_input.text = server_info["ip"]
+	_port_input.text = str(server_info["port"])
 
 
 func _on_back_button_pressed():
