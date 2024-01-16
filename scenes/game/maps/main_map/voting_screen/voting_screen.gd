bb5c9958823a39fc6e194a63ac2b53bf780f83c0
Kacper Pełka <kacper.pelka03@gmail.com>
2024-01-16 01:11:06 +0100

    Fix tab indentation

--- scenes/game/maps/main_map/voting_screen/voting_screen.gd
+++ scenes/game/maps/main_map/voting_screen/voting_screen.gd
@@ -262,12 +262,12 @@ func _get_most_voted_player_id():
 	var max_vote = 0
 
 	for vote_key in GameManagerSingleton.get_current_game_value("votes").keys():
-			var votes_count = GameManagerSingleton.get_current_game_value("votes")[vote_key].size()
-			if votes_count > max_vote:
-				max_vote = votes_count
-				most_voted_players = [vote_key]
-			elif votes_count == max_vote:
-				most_voted_players.append(vote_key)
+		var votes_count = GameManagerSingleton.get_current_game_value("votes")[vote_key].size()
+		if votes_count > max_vote:
+			max_vote = votes_count
+			most_voted_players = [vote_key]
+		elif votes_count == max_vote:
+			most_voted_players.append(vote_key)
 
 	if most_voted_players.size() > 1 || most_voted_players.size() == 0 || most_voted_players[0] == 0:
 		return null
